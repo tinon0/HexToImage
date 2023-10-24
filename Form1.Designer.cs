234f8a6adb7298c7253b810e2c9ec26390c2ea2f
tinon0 <sagripantivalentino@gmail.com>
2023-10-24 14:55:13 -0300

    Hex Codes to Image WinForm C# Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace HextoJpeg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtHex = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbImagen = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(530, 397);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 22);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Exit";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(491, 49);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtHex
+            // 
+            this.txtHex.Location = new System.Drawing.Point(34, 49);
+            this.txtHex.Name = "txtHex";
+            this.txtHex.Size = new System.Drawing.Size(387, 20);
+            this.txtHex.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Put your Hex code:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbImagen);
+            this.groupBox1.Location = new System.Drawing.Point(34, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(532, 280);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image";
+            // 
+            // pbImagen
+            // 
+            this.pbImagen.Location = new System.Drawing.Point(9, 19);
+            this.pbImagen.Name = "pbImagen";
+            this.pbImagen.Size = new System.Drawing.Size(517, 255);
+            this.pbImagen.TabIndex = 0;
+            this.pbImagen.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 440);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHex);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Hex to Image";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtHex;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbImagen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HextoJpeg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string filepath = "C:\\image.jpeg";
+            File.WriteAllBytes(filepath,ConvertHexToByteArray(txtHex.Text));
+            pbImagen.ImageLocation = filepath;
+
+        }
+
+
+        public byte[] ConvertHexToByteArray(string hexCode)
+        {
+            // Remove whitespace and invalid characters from hexadecimal code
+            hexCode = hexCode.Replace(" ", "").Replace("\r", "").Replace("\n", "");
+
+            // Make sure the length of the string is an even number (each pair of characters represents one byte)
+            if (hexCode.Length % 2 != 0)
+            {
+                throw new ArgumentException("The hexadecimal code must be of even length!");
+            }
+
+            // Create a byte array to store the binary data
+            byte[] bytes = new byte[hexCode.Length / 2];
+
+            // Loop through the hexadecimal string and convert it to bytes
+            for (int i = 0; i < hexCode.Length; i += 2)
+            {
+                string hexFormatByte = hexCode.Substring(i, 2);
+                bytes[i / 2] = Convert.ToByte(hexFormatByte, 16);
+            }
+
+            return bytes;
+        }
+
+    }
+}
